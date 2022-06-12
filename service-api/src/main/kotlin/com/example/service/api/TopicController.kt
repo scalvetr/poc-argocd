c657242dd8fc190995587f98a264b6cb0d30d890
Sergi Calvet <sergi.calvet@gmail.com>
2022-06-12 17:52:18 +0200

    service api:  integration tests

--- service-api/src/main/kotlin/com/example/service/api/TopicController.kt
+++ service-api/src/main/kotlin/com/example/service/api/TopicController.kt
@@ -1,12 +1,9 @@
 package com.example.service.api
 
-import com.example.service.api.service.NotFoundException
-import com.example.service.api.service.ServiceException
 import com.example.service.api.service.TopicService
 import kotlinx.coroutines.flow.Flow
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
-import org.springframework.web.server.ResponseStatusException
 
 @RestController
 @RequestMapping("/v1/topics")
@@ -16,21 +13,14 @@ class TopicController(
     @PostMapping("")
     @ResponseStatus(HttpStatus.CREATED)
     suspend fun createTopic(@RequestHeader("x-tenant") tenant: String, @RequestBody topic: Topic) {
-        try {
-            topicService.createTopic(tenant, topic)
-        } catch (e: RuntimeException) {
-            throw handle(e)
-        }
+        topicService.createTopic(tenant, topic)
     }
 
     @PutMapping("")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     suspend fun updateTopic(@RequestHeader("x-tenant") tenant: String, @RequestBody topic: Topic) {
-        try {
-            topicService.updateTopic(tenant, topic)
-        } catch (e: RuntimeException) {
-            throw handle(e)
-        }
+
+        topicService.updateTopic(tenant, topic)
     }
 
     @GetMapping("/{topicName}")
@@ -39,39 +29,18 @@ class TopicController(
         @RequestHeader("x-tenant") tenant: String,
         @RequestParam("topicName") topicName: String
     ): Topic {
-        try {
-            return topicService.getTopic(tenant, topicName)
-        } catch (e: RuntimeException) {
-            throw handle(e)
-        }
+        return topicService.getTopic(tenant, topicName)
     }
 
     @DeleteMapping("/{topicName}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     suspend fun deleteTopic(@RequestHeader("x-tenant") tenant: String, @RequestParam("topicName") topicName: String) {
-        try {
-            topicService.deleteTopic(tenant, topicName)
-        } catch (e: RuntimeException) {
-            throw handle(e)
-        }
+        topicService.deleteTopic(tenant, topicName)
     }
 
     @GetMapping("")
     @ResponseStatus(HttpStatus.OK)
     fun getTopics(@RequestHeader("x-tenant") tenant: String): Flow<Topic> {
-        try {
-            return topicService.getTopics(tenant)
-        } catch (e: RuntimeException) {
-            throw handle(e)
-        }
-    }
-
-    fun handle(e: RuntimeException): ResponseStatusException {
-        if (e is NotFoundException) {
-            return ResponseStatusException(HttpStatus.NOT_FOUND, e.message)
-        } else if (e is ServiceException) {
-            return ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY, e.message)
-        }
-        return ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, e.message)
+        return topicService.getTopics(tenant)
     }
 }
